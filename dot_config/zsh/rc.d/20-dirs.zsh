6edd59df31f2ed648ab0cea6a784f0606169c4cd
Nikita Karamov <me@kytta.dev>
2022-02-10 17:21:23 +0100

    Update dev shortcuts

--- dot_config/zsh/rc.d/20-dirs.zsh
+++ dot_config/zsh/rc.d/20-dirs.zsh
@@ -5,7 +5,9 @@ setopt AUTO_CD
 
 # Create shortcuts.
 hash -d z=$ZDOTDIR
-hash -d d=$HOME/dev
-hash -d job=$HOME/dev/jobs
+hash -d de=$HOME/Development
+hash -d dew=$HOME/Development/Work
+hash -d dep=$HOME/Development/Personal
+hash -d dee=$HOME/Development/Education
 hash -d dl=$HOME/Downloads
 hash -d chezmoi="$(chezmoi source-path)"
