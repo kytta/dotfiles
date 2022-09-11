7ed3734be969bfb5db2ff4089435a53122726c07
Nikita Karamov <me@kytta.dev>
2022-09-11 13:23:12 +0200

    Add script for `defaults_write` on macOS

---
+++ run_once_defaults-write.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Enable old-style alert windows
+defaults write -g NSAlertMetricsGatheringEnabled -bool false
+
+# Auto-hide Dock & adjust speeds
+defaults write "com.apple.Dock" "showhidden" -bool TRUE
+defaults write "com.apple.Dock" "autohide-time-modifier" -float 0.5
+defaults write "com.apple.Dock" "autohide-delay" -float 0
+killall Dock
