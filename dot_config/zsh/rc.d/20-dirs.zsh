343424a2eee7835c5f3d34ce595fec8398746758
Nikita Karamov <me@kytta.dev>
2022-09-17 18:22:48 +0200

    Update directories settings

--- dot_config/zsh/rc.d/20-dirs.zsh
+++ dot_config/zsh/rc.d/20-dirs.zsh
@@ -1,9 +1,33 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
+# Directories and their respective options
+
+
 
 # Change dirs without `cd`
 setopt AUTO_CD
 
-# Create shortcuts.
+# Push the old directory onto the stack on cd.
+setopt AUTO_PUSHD
+
+# Do not store duplicates in the stack.
+setopt PUSHD_IGNORE_DUPS
+
+# Do not print the directory stack after pushd or popd.
+setopt PUSHD_SILENT
+
+# Push to home directory when no argument is given.
+setopt PUSHD_TO_HOME
+
+# Aliases for quick navigation
+alias d='dirs -v'
+for index ({1..9}) alias "$index"="cd +${index}"; unset index
+
+
+
+#
+# Shortcuts
+#
+
 hash -d z=$ZDOTDIR
 hash -d c=$HOME/Code
 hash -d dl=$HOME/Downloads
