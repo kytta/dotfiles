0e7bc69c1bac02c7ee23b2863d586a88a50ace6d
Nikita Karamov <me@kytta.dev>
2023-01-06 23:23:53 +0100

    chezmoi: ignore scripts inside scripts

--- run_once_before_install-homebrew.sh
+++ run_once_before_install-homebrew.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+{{ if eq .chezmoi.os "darwin" }}
 if ! command -v brew > /dev/null; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+{{ end }}
--- run_once_defaults-write.sh
+++ run_once_defaults-write.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+{{ if eq .chezmoi.os "darwin" }}
 # Enable old-style alert windows
 defaults write -g NSAlertMetricsGatheringEnabled -bool false
 
@@ -8,3 +9,4 @@ defaults write "com.apple.Dock" "showhidden" -bool TRUE
 defaults write "com.apple.Dock" "autohide-time-modifier" -float 0.5
 defaults write "com.apple.Dock" "autohide-delay" -float 0
 killall Dock
+{{ end }}
