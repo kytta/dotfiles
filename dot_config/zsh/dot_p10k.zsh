c294c7cdd72c1cc6118afd88c2162b3d28bafe13
Nikita Karamov <me@kytta.dev>
2023-07-23 16:38:14 +0200

    p10k: change font mode

--- dot_config/zsh/dot_p10k.zsh
+++ dot_config/zsh/dot_p10k.zsh
@@ -34,7 +34,7 @@ typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
 typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
 
 # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
-typeset -g POWERLEVEL9K_MODE=nerdfont-complete
+typeset -g POWERLEVEL9K_MODE=powerline
 # When set to `moderate`, some icons will have an extra space after them. This is meant to avoid
 # icon overlap when using non-monospace fonts. When set to `none`, spaces are not added.
 typeset -g POWERLEVEL9K_ICON_PADDING=none
